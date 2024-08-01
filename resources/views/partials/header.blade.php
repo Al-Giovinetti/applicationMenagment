c7e28b95446de7d51d388a9a573c0ce7107da40e
Al-Giovinetti <giovinettialessio@gmail.com>
2024-08-01 19:31:56 +0200

    added style to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,53 @@
 <header>
-    <img src="" alt="" srcset="">
-    <nav>
-        <ul>
-            <li>candidature</li>
-            <li>aggiungi</li>
-            <li>eliminati</li>
-        </ul>
-    </nav>
+    <div class="container">
+        <div class="row justify-content-between align-items-center align-items-sm-end">
+            <div class="col-3 col-md-5">
+                <img  src="{{ asset('media/peraJob.jpg') }}" alt="logo">
+            </div>
+            <div class="col-5 text-end col-sm-7">
+                <div class="d-none h-100 d-sm-flex flex-column justify-content-end">
+                    <nav>
+                        <ul class="d-flex justify-content-between list-unstyled">
+                            <li>
+                                <a href="#">candidature</a>
+                            </li>
+                            <li>
+                                <a href="#">aggiungi</a>
+                            </li>
+                            <li>
+                                <a href="#">Eliminati</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <p class="text-center d-none d-sm-block mb-1 mt-4">Inserisci e registra le tue candidature</p>
+                </div>
+                <i class="fa-solid fa-burger"></i>
+            </div>
+            <nav class="d-none d-sm-none nav-collapse">
+                <ul class="d-flex justify-content-around list-unstyled">
+                    <li>
+                        <a href="#">candidature</a>
+                    </li>
+                    <li>
+                        <a href="#">aggiungi</a>
+                    </li>
+                    <li>
+                        <a href="#">Eliminati</a>
+                    </li>
+                </ul> 
+            </nav>
+        </div>
+    </div>
 </header>
+
+<script>
+    let headerHamburger = document.querySelector('header i');
+    headerHamburger.addEventListener('click',function(){
+        const navCollapse = document.querySelector('nav.nav-collapse');
+        if(navCollapse.classList.contains('d-none')){
+            navCollapse.classList.remove('d-none');
+        }else{
+            navCollapse.classList.add('d-none');
+        }
+    })
+</script>
