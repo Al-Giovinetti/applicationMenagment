61f60c76c1ab02fcf935bc7defe3715f48a3a281
Al-Giovinetti <giovinettialessio@gmail.com>
2024-08-05 17:49:27 +0200

    index page created

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\jobApplication;
+
 class PageController extends Controller
 {
     public function goToIndexPage(){
-        return view('applications.index');
+        $applications = jobApplication::orderBy('id','desc')->get();
+
+        return view('applications.index', compact('applications'));
     }
 
     public function goToCreatePage(){
--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -2,7 +2,35 @@
 
 @section('title','application list')
 
+@php
+
+    function convertData($data){
+        $createDate = new DateTime($data);
+        $strip = $createDate->format('Y-m-d');
+        return $strip;
+    }
+
+@endphp
+
 @section('content')
-    corpo della pagina
+<div class="container">
+    <h2>Lista completa delle tue candidature</h2>
+    <div class="row">
+        @foreach ($applications as $application)
+            <div class="col-12 col-md-6 col-lg-4 d-flex">
+                <div class="card p-3 pb-5 w-100 my-2">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h5>{{ $application['agency_name'] }}</h5>
+                        <p>{{ $application['agency_place'] }}</p>
+                    </div>
+                    <p>Competenze : {{ $application['skills']}} </p>
+                    <p> {{ $application['extra_info'] }}</p>
+                    <p class="text-center application-date"> Dati di applicazione : {{ convertData($application['created_at']) }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+    
 @endsection
     
